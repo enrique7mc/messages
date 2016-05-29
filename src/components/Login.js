c5ef67b3390416cae38732664c4f01e9c7ca84b1
enrique7mc <enrique7mc@gmail.com>
2016-05-29 09:59:11 -0700

    Refactor Login and Message to stateless components

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,27 +3,27 @@ import Actions from '../actions';
 import { Card, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 
-export default class Login extends Component {
-  onClick(){
+const Login = (props) => {
+  function onClick () {
     Actions.login();
   }
 
-  render(){
-    let cardStyle = {
-      'maxWidth': '800px',
-      'margin': '30px auto',
-      'padding': '50px'
-    };
+  let cardStyle = {
+    'maxWidth': '800px',
+    'margin': '30px auto',
+    'padding': '50px'
+  };
 
-    return (
-      <Card style={ cardStyle }>
-        <CardText style={{ 'textAlign': 'center' }}>
-          To start chatting away, please log in with your Google account.
-        </CardText>
-        <RaisedButton style={{ display: 'block', }}
-                      onClick={this.onClick.bind(this)}
-                      label="Log in with Google" primary={true} />
-      </Card>
-    );
-  }
+  return (
+    <Card style={ cardStyle }>
+      <CardText style={{ 'textAlign': 'center' }}>
+        To start chatting away, please log in with your Google account.
+      </CardText>
+      <RaisedButton style={{ display: 'block', }}
+                    onClick={ onClick }
+                    label="Log in with Google" primary={ true } />
+    </Card>
+  );
 }
+
+export default Login;
--- src/components/Message.js
+++ src/components/Message.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import Avatar from 'material-ui/Avatar';
 import { ListItem } from 'material-ui/List';
 
-export default class Message extends Component {
-  constructor (props) {
-    super(props);
-  }
-
-  render () {
-    return (
-      <ListItem
-        leftAvatar={<Avatar src='https://placehold.it/100x100' />}
-      >{ this.props.message }</ListItem>
-    );
-  }
+const Message = (props) => {
+  return (
+    <ListItem
+      leftAvatar={<Avatar src='https://placehold.it/100x100' />}>
+      { props.message }
+    </ListItem>
+  );
 }
+
+export default Message;
